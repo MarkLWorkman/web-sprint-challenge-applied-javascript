eb23022876df6c15e92c57ea05de8480620a0301
unknown <calanhaga@aim.com>
2021-01-22 23:20:38 -0500

    Task 5 and 6 complete, MVP

--- src/components/card.js
+++ src/components/card.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const Card = (article) => {
   // TASK 5
   // ---------------------
@@ -17,6 +19,32 @@ const Card = (article) => {
   //   </div>
   // </div>
   //
+  const card = document.createElement('div');
+  const headline = document.createElement('div');
+  const author = document.createElement('div');
+  const imgContainer = document.createElement('div');
+  const img = document.createElement('img');
+  const byAuthor = document.createElement('span');
+
+  card.classList.add('card');
+  headline.classList.add('headline');
+  author.classList.add('author');
+  imgContainer.classList.add('img-container');
+
+  card.appendChild(headline);
+  card.appendChild(author);
+  author.appendChild(imgContainer);
+  author.appendChild(byAuthor);
+  imgContainer.appendChild(img);
+
+  headline.textContent = article.headline;
+  byAuthor.textContent = article.authorName;
+  img.src = article.authorPhoto;
+
+  card.addEventListener('click', (event) => {
+    console.log(headline.textContent);
+  })
+  return card;
 }
 
 const cardAppender = (selector) => {
@@ -28,6 +56,21 @@ const cardAppender = (selector) => {
   // Create a card from each and every article object in the response, using the Card component.
   // Append each card to the element in the DOM that matches the selector passed to the function.
   //
+  axios.get('https://lambda-times-api.herokuapp.com/topics')
+  .then(res => {
+    res.data.topics.forEach(element => {
+
+      axios.get('https://lambda-times-api.herokuapp.com/articles')
+      .then(res => {
+        const info = document.querySelector(selector);
+        const cardDisplay = res.data.articles[element];
+
+        for (let i = 0; i < cardDisplay.length; i++) {
+          info.append(Card(cardDisplay[i]));
+        }
+      })
+    })
+  })
 }
 
 export { Card, cardAppender }
--- src/components/header.js
+++ src/components/header.js
@@ -39,7 +39,6 @@ const headerAppender = (selector) => {
   // It should append the header to the element in the DOM that matches the given selector.
   //
   const headerContainer = document.querySelector(selector);
-  headerContainer.append(Header('Test Title', '1/22/2021', '30'))
 }
 
 export { Header, headerAppender }
